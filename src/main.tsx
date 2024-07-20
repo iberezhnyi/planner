c94c8fec53e8c06921aa098c39e9ed06e10aee8b
Ihor Berezhnyi <iberezhnyi@gmail.com>
2024-07-20 18:39:57 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Theme>
-          <BrowserRouter>
+          <BrowserRouter basename="/mv">
             <App />
           </BrowserRouter>
         </Theme>
